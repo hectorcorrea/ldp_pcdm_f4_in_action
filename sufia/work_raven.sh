ebd6e22edd37351a994879b10976a36c74ecd90e
Hector Correa <hector@hectorcorrea.com>
2015-05-01 16:41:49 -0400

    Add work to collection

--- sufia/work_raven.sh
+++ sufia/work_raven.sh
@@ -52,3 +52,17 @@ curl -i -XPUT -H "Content-Type: image/png"        --data-binary @raven_thumb.png
 curl -i -XPUT -H "Content-Type: text/plain"       --data-binary @raven.txt $BASE/works/raven/genfiles/raven.pdf/files/text
 
 
+#
+# COLLECTIONS
+#
+# Collection root
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @collections.ttl $BASE/collections/
+
+# Add one collection (poe)
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @poe.ttl $BASE/collections/poe
+
+# Add an indirect container to help with adding existing works to the collection
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @poe-members.ttl $BASE/collections/poe/members
+
+# Add the Raven work to the Poe collection
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @raven-proxy.ttl $BASE/collections/poe/members/ravenProxy
