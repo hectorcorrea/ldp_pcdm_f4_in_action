d77d9cc9ccf0641a42343782cb3bd10514c70d17
Hector Correa <hector@hectorcorrea.com>
2015-05-01 16:16:21 -0400

    Multiple files for a single work

--- sufia/work_raven.sh
+++ sufia/work_raven.sh
@@ -14,26 +14,41 @@ curl -X PUT ${BASE}
 # 
 
 # Works root
-curl -i -XPUT -H    "Content-Type: text/turtle" --data-binary @works.ttl $BASE/works/
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @works.ttl $BASE/works/
 
-# One work (raven)
-curl -i -XPUT -H    "Content-Type: text/turtle" --data-binary @raven.ttl $BASE/works/raven/
+# Add one work (raven)
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @raven.ttl $BASE/works/raven/
 
-# Add a direct container to hold the files for the work 
-curl -i -XPUT -H    "Content-Type: text/turtle" --data-binary @raven-genfiles.ttl $BASE/works/raven/genfiles
+# ...and a direct container to hold the files of the work 
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @raven-genfiles.ttl $BASE/works/raven/genfiles
+
 
 # Add an object to represent the file (this is the Sufia GenericFile)
 # This is not the actual file, this is a pcdm:Object
-curl -i -XPUT -H    "Content-Type: text/turtle" --data-binary @cover.ttl $BASE/works/raven/genfiles/cover.jpg
+# This object has descriptive metadata (e.g. dc:title)
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @cover.ttl $BASE/works/raven/genfiles/cover.jpg
 
 # Add a direct container to hold the actual files for the generic file
-curl -i -XPUT -H    "Content-Type: text/turtle" --data-binary @cover-files.ttl $BASE/works/raven/genfiles/cover.jpg/files
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @cover-files.ttl $BASE/works/raven/genfiles/cover.jpg/files
 
 # Add an actual file to the GenericFile (cover.jpg) via the direct container (cover.jpg/files)
-curl -i -XPUT -H    "Content-Type: image/jpeg"  --data-binary @cover.jpg $BASE/works/raven/genfiles/cover.jpg/files/content
-# TODO: add thumbnail for file $BASE/works/raven/genfiles/cover.jpg/files/thumbnail
+# These objects should pcdm:File (todo: add extra curl call to patch them)
+# These objects do NOT have descriptive metadata
+curl -i -XPUT -H "Content-Type: image/jpeg"  --data-binary @cover.jpg $BASE/works/raven/genfiles/cover.jpg/files/content
+curl -i -XPUT -H "Content-Type: image/jpeg"  --data-binary @cover_thumb.jpg $BASE/works/raven/genfiles/cover.jpg/files/thumbnail
 
-# TODO: add a second file $BASE/works/raven/genfiles/firstedition.pdf/files/content
-# TODO: add a second file $BASE/works/raven/genfiles/firstedition.pdf/files/thumbnail
+
+# Add an object represent another file inside the work (again, this is a Sufia GenericFile)
+# This is not the actual file, this is a pcdm:Object
+# And will have descriptive metadata
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @raven-pdf.ttl $BASE/works/raven/genfiles/raven.pdf
+
+# Add a direct container to hold the actual files for the generic file
+curl -i -XPUT -H "Content-Type: text/turtle" --data-binary @raven-pdf-files.ttl $BASE/works/raven/genfiles/raven.pdf/files
+
+# Add three files to the GenericFile (raven.pdf) via the direct container (raven.pdf/files)
+curl -i -XPUT -H "Content-Type: application/pdf"  --data-binary @raven.pdf $BASE/works/raven/genfiles/raven.pdf/files/content
+curl -i -XPUT -H "Content-Type: image/png"        --data-binary @raven_thumb.png $BASE/works/raven/genfiles/raven.pdf/files/thumbnail
+curl -i -XPUT -H "Content-Type: text/plain"       --data-binary @raven.txt $BASE/works/raven/genfiles/raven.pdf/files/text
 
 
